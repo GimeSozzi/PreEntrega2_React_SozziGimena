bd7492655da85f055249c89d315384a82c70f856
Gime Sozzi <sozzi.gime@gmail.com>
2024-11-12 20:14:33 -0300

    actualizo NavBar y ButtonCategory

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,3 @@
-// import { Navbar, Container, Nav } from 'react-bootstrap';
-// import CartWidget from './CartWidget';
-// import Brand from './Brand';
-// import ButtonCategory from './ButtonCategory';
-// import './NavBar.css';
-
-// function NavBar() {
-//   const categories = ['Todo', 'Comidas', 'Postres', 'Bebidas', 'Cafeteria'];
-
-//   return (
-//     <Navbar expand="lg" className="custom-navbar">
-//       <Container className="d-flex justify-content-between align-items-center">
-//         <Brand />
-//         <Nav className="justify-content-center flex-grow-1">
-//           {categories.map((category) => (
-//             <ButtonCategory key={category} categoryName={category} />
-//           ))}
-//         </Nav>
-//         <CartWidget />
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default NavBar;
-
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CartWidget from './CartWidget';
